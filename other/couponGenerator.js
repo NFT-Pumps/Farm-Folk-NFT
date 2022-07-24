a7a393633a1da5936108fb90cd0bc7fa51bfd916
greene5952 <dustin@wolf-solutions.us>
2022-07-23 19:24:55 -0500

    WL Update

--- other/couponGenerator.js
+++ other/couponGenerator.js
@@ -46,6 +46,7 @@ async function getClaimCodes() {
         { address : '0xbF9A9d3D1A330AE27c5d9632A17F20D129c40209', qty : 4},
         { address : '0x23AEb45378AD26437cE1c3dD0ac4A015E9865596', qty : 4},
         { address : '0xc5915dd28886523DCf96Bc29B5bFb1a77c4166D8', qty : 4},
+        { address : '0xa7531F5A9D56089A79EBCb295bAba41bED80ca22', qty : 4},
         { address : '0xf3C11bA805765Bd0aFce51f605FfDe875e04ef17', qty : 4}       
     ]      
     
