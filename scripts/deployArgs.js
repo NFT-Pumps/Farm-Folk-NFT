d27e5fffcb149d7156237cac1e1e1e1eddae5e79
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-10 12:16:37 -0500

    ReMint Initial

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -1,26 +1,26 @@
 module.exports = [
- 'Farm Folk NFT',
- 'FFNFT',
+ 'The Kreamer Collection',
+ 'TKCNFT',
  
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
-    'https://public-pre-ipfs.s3.amazonaws.com/Farm_Folk_NFT/json/',
-    'https://public-pre-ipfs.s3.amazonaws.com/Farm_Folk_NFT/assets/FarmFolkReveal.json',
+    'https://public-pre-ipfs.s3.amazonaws.com/TheKreamerCollection/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/TheKreamerCollection/assets/KreamerReveal.json',
 
     [
-        ethers.utils.getAddress('0xC3ea7494c33AbCA4FcEa01331043311756b7e336'),
+        ethers.utils.getAddress('0xd708BA3dA864cC6E8596d3E53d3468bAB54Be9b0'),
         ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989'),
         ethers.utils.getAddress('0x6b3ba9F0475C646FDaF2Bc207C285a485B88A747'),
-        ethers.utils.getAddress('0xbE400D9A6f8140B51dE4B3b7AAb1DA4B9adec9a0'),
-        ethers.utils.getAddress('0xEAC3ce292F95d779732e7a26c95c57A742cf5119'),
-        ethers.utils.getAddress('0x9Dc7D54E10A199C00176FFA68459fD9b15A706c4')
+        ethers.utils.getAddress('0xE4cD887b35C7a7941EEbE8336c6Bfc5d1E9f7bfA'),
+        ethers.utils.getAddress('0x744193D5f234183FC821DfB0ACBb6eB8a1792Bc2'),
+        ethers.utils.getAddress('0x2E00F4Dd34c0A9aab2cd64B046469336A4914870')
     ],
     [
         26,
         12,
         10,
         6,
-        5,
-        41
+        6,
+        40
     ]
 ]
